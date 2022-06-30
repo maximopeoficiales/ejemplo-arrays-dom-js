03f44eea33ce478b1e6118c41056048010c3ee7a
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-06-30 13:14:25 -0500

    ADD: falta logica del localstorage

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,41 @@
 const arraysTodos = [];
 const listTodoElement = document.querySelector("#list-todo");
+const inputTodo = document.querySelector("#todo-input");
+
 // agrego un evento al formulario
-function addTodo(newTodo) {
+const addTodo = (newTodo = "") => {
     const elementA = document.createElement("a");
     elementA.textContent = newTodo;
-    arraysTodos.push(newTodo);
     elementA.classList.add("list-group-item", "list-group-item-action");
     listTodoElement.appendChild(elementA);
 }
-function addTodos(todos = []) {
+
+const addTodos = (todos = []) => {
     todos.forEach(e => {
         addTodo(e);
     });
 }
+
+const getLocalStorage = (key = "") => {
+    return JSON.parse(localStorage.getItem(key) ?? "");
+}
+const saveLocalStorage = (key = "", value) => {
+    
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
 document.querySelector("#form-todo").addEventListener("submit", (e) => {
     // prevengo su evento por defecto por la tanto no recarga
     e.preventDefault();
     // obtengo valores del input
-    const inputTodo = document.querySelector("#todo-input");
+
     const inputTodoText = inputTodo.value.replace(" ", "");
     // <a href="#" class="list-group-item list-group-item-action">Item</a>
     if (inputTodoText.length > 0) {
         // si tiene valores
+        arraysTodos.push(inputTodoText);
         addTodo(inputTodoText);
-
+        saveLocalStorage("mysTodos", arraysTodos);
     } else {
         // muestro una alertaa
         alert("El input esta vacio")
@@ -31,16 +43,29 @@ document.querySelector("#form-todo").addEventListener("submit", (e) => {
 });
 
 document.querySelector("#search-input").addEventListener("keyup", (e) => {
+    // console.log(arraysTodos);
     const searchText = e.target.value;
+    // si no esta vacio el input
     if (searchText.length > 0) {
-        const findResults = arraysTodos.filter(e => e === searchText);
+        // busco el las coincidencias en el array
+        const findResults = arraysTodos.filter((e) => e === searchText);
+        // hay resultados de la busqueda
         if (findResults.length > 0) {
-            // hay resultados
+            // vacio la lista de todos
             listTodoElement.innerHTML = "";
-            // si tiene valores
+            // renderizo los resultados encontrados 
             addTodos(findResults)
 
         }
+    } else {
+        // si esta vacio
+        // elimino los elementos hijos
+        listTodoElement.innerHTML = "";
+        // renderizo en memoria
+        addTodos(arraysTodos);
     }
 
 });
+
+
+// como guardarlo en el locastorage
